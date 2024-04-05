c00a897f3aed45d8903c671699206e0ed2b655f7
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-05 15:57:06 +0400

    index.css yaratdix

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,37 +1,20 @@
 import React, { useState } from "react"
-
+import "./index.css"
 const Index = ({ }) => {
     const [claculator, setClaculator] = useState('')
     const [claculator2, setClaculator2] = useState('')
-    const [result, setResult] = useState('')
-
+    // const [result, setResult] = useState('')
 
 
     return (
         <>
-
-            <input
-                type="text"
-                onChange={(e) => {
-                    setClaculator(e.target.value)
-                }}
-            /><br />
-
-            <input type="text"
-                onChange={(e) => {
-                    setClaculator2(e.target.value)
-                }}
-                style={{ marginTop: "5px" }} />
-
+            <input type="text" onChange={(e) => {setClaculator(e.target.value)}}/><br />
+            <input type="text" onChange={(e) => {setClaculator2(e.target.value)}} style={{ marginTop: "5px" }} />
             <button>click</button>
-
-
             <p>sum: {+claculator + +claculator2}</p>
             <p>minus: {+claculator - +claculator2}</p>
             <p>multiply: {+claculator * +claculator2}</p>
             <p>divide: {+claculator2 === 0 ? 'cannot divide by 0' : (+claculator / +claculator2)}</p>
-
-
         </>
     )
 }
