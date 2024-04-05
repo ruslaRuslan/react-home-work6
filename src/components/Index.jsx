07a1bc6fa7c36431006fdca03e86707c273c92cb
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-05 15:26:17 +0400

    sum hazir

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react"
 
 const Index = ({ }) => {
-    const [claculator, setClaculator] = useState(second)
+    const [claculator, setClaculator] = useState('')
+    const [claculator2, setClaculator2] = useState('')
+    const [result, setResult] = useState('')
+
+
+    
+
+
+
+
     return (
-        <div>Index</div>
+        <>
+
+            <input
+                type="text"
+                value={claculator}
+                onChange={(e) => {
+                    setClaculator(e.target.value)
+                }}
+            /><br />
+
+            <input type="text"
+                value={claculator2}
+                onChange={(e) => {
+                    setClaculator2(e.target.value)
+                }}
+                style={{ marginTop: "5px" }} />
+
+            <button>click</button>
+
+
+            <p>{+claculator + +claculator2}</p>
+
+        </>
     )
 }
 
