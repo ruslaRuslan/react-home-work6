7299fd144e3583a9a51125f63d4a02397efbe4c4
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-05 16:44:06 +0400

    is tamamen hazir infinity ve Nan olmayacax

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react"
 import "./index.css"
 const Index = ({ }) => {
-    const [claculator, setClaculator] = useState('')
-    const [claculator2, setClaculator2] = useState('')
+    const [claculator, setClaculator] = useState(0)
+    const [claculator2, setClaculator2] = useState(0)
     return (
         <>
             <div>
-                <input type="text" onChange={(e) => { setClaculator(e.target.value) }} /><br />
-                <input className="input" type="text" onChange={(e) => { setClaculator2(e.target.value) }} />
+                <input type="text" onChange={(e) => { setClaculator(+e.target.value) }} /><br />
+                <input className="input" type="text" onChange={(e) => { setClaculator2(+e.target.value) }} />
                 <br />
                 <button onClick={() => {
-                    console.log(`sum: ${+claculator + +claculator2} minus: ${+claculator - +claculator2} multipy: ${+claculator * +claculator2} divide:${+claculator / +claculator2}`)
+                    console.log(`sum: ${claculator + claculator2} minus: ${claculator - claculator2} multipy: ${claculator * claculator2} divide:${claculator / claculator2}`)
                 }}>click</button>
-                <p>sum: {+claculator + +claculator2}</p>
-                <p>minus: {+claculator - +claculator2}</p>
-                <p>multiply: {+claculator * +claculator2}</p>
-                <p>divide: {+claculator2 === 0 ? 'cannot divide by 0' : (+claculator / +claculator2)}</p>
+                <p>sum: {claculator + claculator2}</p>
+                <p>minus: {claculator - claculator2}</p>
+                <p>multiply: {claculator * claculator2}</p>
+                <p>divide: {claculator2 === 0 ? 'cannot divide by 0' : (claculator / claculator2)}</p>
             </div>
         </>
 
