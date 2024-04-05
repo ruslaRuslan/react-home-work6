b9ae3f7d6ab38414b0758f40adbc2af435567461
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-05 14:08:08 +0400

    import-lar hazir

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-
+import Index from "./components/Index"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <Index />
     </>
   )
 }
---
+++ src/components/Index.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react"
+
+const Index = ({ }) => {
+    const [claculator, setClaculator] = useState(second)
+    return (
+        <div>Index</div>
+    )
+}
+
+export default Index
